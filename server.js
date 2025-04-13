3c38a1ae975652b4c32e426607ecb3b54d345e14
MyNeko1 <130404842+MyNeko1@users.noreply.github.com>
2025-04-13 14:41:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -224,7 +224,7 @@ wss.on('connection', (ws) => {
     queueUpdate({ type: 'leave', id });
   });
 });
-app.use('/client', express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '.')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
