5b19f883db30cd2516e274da04d0939f6f1b0824
MyNeko1 <130404842+MyNeko1@users.noreply.github.com>
2025-04-14 12:49:23 +0300

    Update items.js

--- items.js
+++ items.js
@@ -1,34 +1,15 @@
-const items = [
-  { type: "blocks", name: "Блоки", count: 0, action: (player, targetX, targetY, ws, nearby, getCell, inventory) => {
-    if (nearby(player.x, player.y, targetX, targetY, 2) && getCell(targetX, targetY) == 0 && inventory[0].count > 0) {
-      ws.send(JSON.stringify({ type: 'build', x: targetX, y: targetY }));
+const items = {
+  stick: {
+    name: "Палка",
+    isHeld: false,
+    toggle: function() {
+      this.isHeld = !this.isHeld;
+      return this.isHeld;
+    },
+    getState: function() {
+      return this.isHeld;
     }
-  }},
-  { type: "sword", name: "Меч", action: (player, players, ws, nearby, myId) => {
-    for (let [id, otherPlayer] of players) {
-      if (id !== myId && nearby(player.x, player.y, otherPlayer.x, otherPlayer.y, 1)) {
-        ws.send(JSON.stringify({ type: 'damage', targetId: id, damage: 10 }));
-        break;
-      }
-    }
-  }},
-  { type: "pickaxe", name: "Кирка", action: (player, targetX, targetY, ws, nearby, getCell) => {
-    if (nearby(player.x, player.y, targetX, targetY, 1) && getCell(targetX, targetY) == 1) {
-      ws.send(JSON.stringify({ type: 'break', x: targetX, y: targetY }));
-    }
-  }},
-  { type: "bow", name: "Лук", action: (player, targetX, targetY, ws, myId) => {
-    ws.send(JSON.stringify({ type: 'arrow', x: player.x, y: player.y, targetX, targetY, shooterId: myId }));
-  }},
-  { type: "food", name: "Еда", action: (player, ws) => {
-    ws.send(JSON.stringify({ type: 'heal', amount: 10 }));
-  }}
-];
+  }
+};
 
-function getItems() {
-  return items;
-}
-
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = { getItems };
-}
+export default items;
